bd02fa8a32e4c68ed013182bf87f4ec90ef1ae45
Mohammad <mohamed.hallumi@zadarastorage.com>
2025-08-02 17:21:10 +0300

    Update configuration for new repository name 'miami'

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/miami-sea-feast">
+      <BrowserRouter basename="/miami">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
